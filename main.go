8fcc10f0581a9c22aa1e93ecadf50a03fe77e1c3
archiblue22 <paguirre@byui.edu>
2022-01-21 23:05:42 -0700

    commit

--- main.go
+++ main.go
@@ -11,4 +11,21 @@ func main() {
 
 	log.Println("Listening on :3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
+
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logged-in", loggedIn)
+	http.HandleFunc("/logout", logout)
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func loggedIn(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+
 }
